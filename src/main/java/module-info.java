8b7c30ff8411ff44cf3c7cd6ae27b79fb9e9db2a
Alec Redmond <akossedevamp@gmail.com>
2026-02-22 13:09:32 +0000

    - Fixed formatting in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,10 @@ module BayesSolver {
   requires static lombok;
   requires org.slf4j;
   requires java.desktop;
-    requires java.sql;
-    requires java.rmi;
+  requires java.sql;
+  requires java.rmi;
 
-    exports io.github.alecredmond.application.constraints;
+  exports io.github.alecredmond.application.constraints;
   exports io.github.alecredmond.application.inference;
   exports io.github.alecredmond.application.network;
   exports io.github.alecredmond.application.node;
