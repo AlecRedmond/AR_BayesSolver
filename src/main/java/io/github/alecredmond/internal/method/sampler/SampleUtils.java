9aa44e91254f36b2105ae5a853a7806121e161d4
Alec Redmond <akossedevamp@gmail.com>
2026-05-27 17:05:22 +0100

    - SampleCollection and Sample now export as interfaces, with their implementations moved to the internal package - Fewer methods in SampleCollection and Sample - InferenceEngine no longer required to construct a Sampler, but may be used in a Sampler method to sample from the current observations - Sampler can now be constructed from within BayesianNetwork - Expanded SampleCollectionTest to cover all methods in SampleCollection and Sample - Added InferenceEngine overloaded methods for single-item requests

--- src/main/java/io/github/alecredmond/internal/method/sampler/SampleUtils.java
+++ src/main/java/io/github/alecredmond/internal/method/sampler/SampleUtils.java
@@ -2,44 +2,13 @@ package io.github.alecredmond.internal.method.sampler;
 
 import io.github.alecredmond.export.application.node.Node;
 import io.github.alecredmond.export.application.node.NodeState;
-import io.github.alecredmond.export.application.sampler.SampleData;
+import io.github.alecredmond.internal.application.sampler.SampleData;
 import java.util.*;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class SampleUtils {
   private SampleUtils() {}
-
-  public static <R extends Collection<NodeState>> void setSampleSupplier(
-      SampleData sampleData, Supplier<R> supplier) {
-    if (supplier.equals(sampleData.getSupplier())) {
-      return;
-    }
-    sampleData.setSupplier(supplier);
-    rebuildStateCollection(sampleData, supplier);
-  }
-
-  public static <T extends Collection<NodeState>, R extends T> T rebuildStateCollection(
-      SampleData sampleData, Supplier<R> supplier) {
-    T newCollection = getStateCollectionCommon(sampleData.getExportStateArray(), supplier, s -> s);
-    sampleData.setStateCollection(newCollection);
-    return newCollection;
-  }
-
-  private static <F extends E, E extends Collection<R>, R> E getStateCollectionCommon(
-      NodeState[] sampledStates,
-      Supplier<F> sampleSupplier,
-      Function<NodeState, R> nodeStateFunction) {
-    return Arrays.stream(sampledStates)
-        .map(nodeStateFunction)
-        .collect(Collectors.toCollection(sampleSupplier));
-  }
-
-  public static <F extends E, E extends Collection<NodeState>> E getStateCollection(
-      NodeState[] sampledStates, Supplier<F> sampleSupplier) {
-    return getStateCollectionCommon(sampledStates, sampleSupplier, nodeState -> nodeState);
-  }
 
   @SuppressWarnings("unchecked")
   public static <T extends Collection<NodeState>, S extends T> T getSampleCollection(
@@ -48,6 +17,19 @@ public class SampleUtils {
       return (T) sampleData.getStateCollection();
     }
     return rebuildStateCollection(sampleData, supplier);
+  }
+
+  public static <T extends Collection<NodeState>, R extends T> T rebuildStateCollection(
+      SampleData sampleData, Supplier<R> supplier) {
+    T newCollection = buildStatesAsCollection(sampleData.getExportStateArray(), supplier);
+    sampleData.setStateCollection(newCollection);
+    sampleData.setSupplier(supplier);
+    return newCollection;
+  }
+
+  public static <F extends E, E extends Collection<NodeState>> E buildStatesAsCollection(
+      NodeState[] sampledStates, Supplier<F> sampleSupplier) {
+    return Arrays.stream(sampledStates).collect(Collectors.toCollection(sampleSupplier));
   }
 
   public static void setExportArrayFromNodes(
@@ -64,12 +46,7 @@ public class SampleUtils {
   }
 
   public static void resetExportArray(SampleData sampleData) {
-    sampleData.setExportStateArray(Arrays.stream(sampleData.getRawStateArray()).toArray(NodeState[]::new));
-  }
-
-  public static <F extends E, E extends Collection<R>, R> E getStateIdCollection(
-      NodeState[] sampledStates, Supplier<F> sampleSupplier, Class<R> idClass) {
-    return getStateCollectionCommon(
-        sampledStates, sampleSupplier, (nodeState -> idClass.cast(nodeState.getId())));
+    sampleData.setExportStateArray(
+        Arrays.stream(sampleData.getRawStateArray()).toArray(NodeState[]::new));
   }
 }
