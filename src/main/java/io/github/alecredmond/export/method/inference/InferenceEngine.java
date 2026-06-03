3445ec2a74523435b3b3c446f788425b7e12b200
Alec Redmond <akossedevamp@gmail.com>
2026-06-03 19:33:18 +0100

    - InferenceEngine javadoc for the class and for InferenceEngine.create(BayesianNetwork network)

--- src/main/java/io/github/alecredmond/export/method/inference/InferenceEngine.java
+++ src/main/java/io/github/alecredmond/export/method/inference/InferenceEngine.java
@@ -4,12 +4,42 @@ import io.github.alecredmond.export.application.node.Node;
 import io.github.alecredmond.export.application.node.NodeState;
 import io.github.alecredmond.export.application.probabilitytables.MarginalTable;
 import io.github.alecredmond.export.method.network.BayesianNetwork;
+import io.github.alecredmond.export.method.sampler.Sampler;
 import io.github.alecredmond.internal.method.inference.InferenceEngineFactory;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * {@code InferenceEngine} provides utilities for running direct inference on a solved {@link
+ * BayesianNetwork}. The values stored in the network's Conditional Probability Tables (CPTs) are
+ * mapped to a secondary inference network which can be queried for joint, marginal, or conditional
+ * probabilities. The documentation for {@link InferenceType} provides further information on the
+ * available variations.
+ *
+ * <p>Specific {@link NodeState} values can be set as observed (always true) using {@link
+ * #observeNetwork(Collection)} or {@link #observeNetworkFromIds(Collection)}. These will be
+ * persisted in the instance until new observations are given, or until the observations are cleared
+ * using {@link #resetObservations()}.
+ *
+ * <p>Unlike Monte Carlo sampling (as used in {@link Sampler}), the results of direct inference are
+ * guaranteed to be correct.
+ *
+ * @see BayesSolver
+ * @author Alec Redmond
+ */
 public interface InferenceEngine {
+
+  /**
+   * Creates an {@code InferenceEngine} from the given {@link BayesianNetwork}, using the default
+   * inference variant configured in {@code app.properties}. The active variant is controlled by
+   * {@code app.inference.useJunctionTreeInference} (default: {@code true}). This will automatically
+   * solve the network if it is unsolved.
+   *
+   * @param network the network where inference is to be run.
+   * @return a new {@code InferenceEngine} instance, or {@code null} if the given network was
+   *     unsolved and could not be solved.
+   */
   static InferenceEngine create(BayesianNetwork network) {
     return new InferenceEngineFactory().create(network);
   }
