aab9f57cd0ce8d554411c40f4d4948984c9aea38
Alec Redmond <akossedevamp@gmail.com>
2026-07-14 18:40:31 +0100

    - InferenceEngine will now only print the tables which don't contribute observed states.

--- src/test/java/io/github/alecredmond/TestConfigs.java
+++ src/test/java/io/github/alecredmond/TestConfigs.java
@@ -2,7 +2,7 @@ package io.github.alecredmond;
 
 public class TestConfigs {
   public static final boolean SOLVE_LONG_TESTS = false;
-  public static final boolean PRINT_TABLES = false;
+  public static final boolean PRINT_TABLES = true;
   public static final boolean SAVE_BINARIES = true;
   public static final boolean RUN_J_FILE_CHOOSER_TESTS = false;
   public static final int ALLOWED_STDEV = 3;
